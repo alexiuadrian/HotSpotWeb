5470bdba7680dccbd4b859483bfab8c1ce630dd2
alexiuadrian <adi.alexiu@gmail.com>
2023-01-11 15:05:03 +0200

    fixed application functionality

---
+++ aspnet-core/src/HotSpotWeb.EntityFrameworkCore/Migrations/20221229082452_AddApplication.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotSpotWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: true),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VersionControl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RepositoryUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RepositoryUsername = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RepositoryBranch = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Technology = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TenantId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dependency",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OfficialUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TargetFramework = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApplicationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dependency", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dependency_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dependency_ApplicationId",
+                table: "Dependency",
+                column: "ApplicationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dependency");
+
+            migrationBuilder.DropTable(
+                name: "Applications");
+        }
+    }
+}
---
+++ aspnet-core/src/HotSpotWeb.EntityFrameworkCore/Migrations/20221229173342_ChangedTenantIdToUserId.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotSpotWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedTenantIdToUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Applications");
+
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "Applications",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Applications");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TenantId",
+                table: "Applications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
