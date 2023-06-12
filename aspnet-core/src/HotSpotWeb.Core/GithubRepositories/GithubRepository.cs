b19df46db870da36617901aec150c3f66a892487
Adrian Alexiu <adi.alexiu@gmail.com>
2023-06-12 20:04:48 +0300

    added githubRepository class

---
+++ aspnet-core/src/HotSpotWeb.Core/GithubRepositories/GithubRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotSpotWeb.GithubProfiles;
+
+namespace HotSpotWeb.GithubRepositories
+{
+    public class GithubRepository
+    {
+        public GithubRepository()
+        {
+
+        }
+
+        public GithubRepository(string repositoryName, string description, GithubProfile githubProfile)
+        {
+            RepositoryName = repositoryName;
+            Description = description;
+            GithubProfile = githubProfile;
+        }
+        
+        public string RepositoryName { get; set; }
+        public string Description { get; set; }
+        public int GithubProfileId { get; set; }
+        public GithubProfile GithubProfile { get; set; }
+
+        public static GithubRepository Create(string repositoryName, string description, GithubProfile githubProfile)
+        {
+            return new GithubRepository(repositoryName, description, githubProfile);
+        }
+    }
+}
